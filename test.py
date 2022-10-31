89b12b5fbf0ee55e2c28364f1497324b1f26c724
Karin Munoz <ksmunoz@superservicios.gov.co>
2022-10-31 14:00:14 -0500

    test update

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
-from Precipitation import Precipitation
-
-# Uso de clase
-p = Precipitation('../Datos/CHIRPS/')
-
-# Calcular y obtener data
+#%%
+from Precipitation import *
+#%% Uso de clase
+p = Precipitation('Datos/CHIRPS/')
+#%%
+# Calcular y obtener data, y exportar a GeoJSON
 export_rain = p.run()
 
-# Exportar a GeoJSON
-p.export_rain()
-
+#%% ------------------------------------------------------------
 from Slidding import Slidding
 
+#%%
 # Uso de clase
-s = Slidding('../Datos/CHIRPS/')
+s = Slidding('Datos/CHIRPS/')
+
+#%%
 # Calcular y obtener data
 deslizamientos = s.run()
-
+#%%
 # Exportar a GeoJSON
 s.export_to_geoJson()
+
+# %%
